ba9deb78c9fdabb6e20f970525275b0fb8f29967
Aditya Kate <z87058556@gmail.com>
2022-12-29 02:34:32 +0530

    Add files via upload

---
+++ book.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+class book{
+    public:
+    string book_name;
+    
+    void getdata(){
+        cout<<"ENter book name : ";
+        cin>>book_name;
+    }
+    void display(){
+        cout<<"Book name : "<<book_name;
+    }
+};
+
+class tape{
+    public:
+    string tape_company;
+    
+    void getdata(){
+        cout<<"ENter tape company name : ";
+        cin>>tape_company;
+    }
+    void display(){
+        cout<<"tape company name : "<<tape_company;
+    }
+    
+};
+
+
+int main(){
+    
+    try{
+        tape t1;
+        book b1;
+        b1.getdata();
+        b1.display();
+        if(b1.book_name == "TTFF"){
+            throw b1.book_name;
+        }else{
+            cout<<" \n Not read yet";
+        }
+    }catch(string book){
+        cout<<" \n Book Already read";
+    }
+    return 0;
+}
